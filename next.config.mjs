88c237ea6863e3acb4bac6b03b3cc5ae9fb70fe9
r6god <61754613+r6god@users.noreply.github.com>
2025-09-24 22:53:45 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
-  images: { unoptimized: true } // so it works out of the box on Vercel/static too
+  images: { unoptimized: true }
 };
 export default nextConfig;
